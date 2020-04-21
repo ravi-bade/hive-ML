bd724580bf09d992a838da57f9fd5142bab4e1e5
Ravikant Bade <ravikant.bade@gmail.com>
2020-04-21 12:02:23 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hive-ml project of OneApp',
+    author='Ravikant Bade',
+    license='MIT',
+)
